2ac3a4d3addfd25d158b8bdfd5fd29bccd7b67b8
evanwheeler <ewheeler@syntergy.com>
2018-02-22 10:31:29 -0500

    version bump

--- test/walker.js
+++ test/walker.js
@@ -1,19 +1,28 @@
-var Bannockburn = require( '..' ),
-    should = require('should/as-function');
+var Bannockburn = require(".."),
+  should = require("should/as-function");
 
 var parser = Bannockburn.Parser(),
-    Walker = Bannockburn.Walker;
+  Walker = Bannockburn.Walker;
 
-describe( "Walker", function() {
-    describe( '#on()', function() {
-        it( "event handlers should be called", function( done ) {
-            ( new Walker() ).on( "FunctionDeclaration", function(node) {
-                if( node.name != 'x' ) {
-                    throw "Invalid name";
-                }
-                done();
-                return false;
-            }).start( parser.parse( "function x(); end" ) );
-        } );
-    } )
-} );
+describe("Walker", function() {
+  describe("#on()", function() {
+    it("event handlers should be called", function(done) {
+      new Walker()
+        .on("FunctionDeclaration", function(node) {
+          if (node.name != "x") {
+            throw "Invalid name";
+          }
+          done();
+          return false;
+        })
+        .start(parser.parse("function x(); end"));
+    });
+    it("event handlers should be called", function(done) {
+      new Walker()
+        .on("before:VariableDeclarator.init", function(node) {
+          done();
+        })
+        .start(parser.parse("function x(); String s = 'asdf' + 'fdsa'; end"));
+    });
+  });
+});
